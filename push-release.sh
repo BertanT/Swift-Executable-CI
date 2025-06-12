3a3df1405e95fd34bca4a210f5a0e68ab9a6c895
Mehmet Bertan Tarakcioglu <83073748+BertanT@users.noreply.github.com>
2025-06-12 15:16:25 +0300

    Fix bug when changelog is disabled but the file exists

--- push-release.sh
+++ push-release.sh
@@ -36,3 +36,7 @@ fi
 # Create and push the new tag
 git tag "${NEW_TAG}"
 git push --atomic origin "${NEW_TAG}"
+
+# Touch the release notes file, this is required even without a changelog since the release action expects a file input.
+# This simpler solution than intordutcing complex conditional changes to the workflow yaml files.
+touch "release-notes-${GITHUB_RUN_ID}.md"
